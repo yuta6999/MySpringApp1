df365dfb38c4f316e27dad0475a81ec4c71cf376
yuta6999 <48405817+yuta6999@users.noreply.github.com>
2024-08-31 05:53:22 +0900

    first commit

---
+++ src/main/java/com/example/myspringapp/MySpringApp1Application.java
@@ -0,0 +1,13 @@
+package com.example.myspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringApp1Application.class, args);
+	}
+
+}
