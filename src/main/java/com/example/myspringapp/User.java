729da445b16c526bfbed10755c586f49d73427b5
yuta6999 <48405817+yuta6999@users.noreply.github.com>
2024-08-31 22:09:13 +0900

    Greeeeeeting

--- src/main/java/com/example/myspringapp/User.java
+++ src/main/java/com/example/myspringapp/User.java
@@ -1,6 +1,5 @@
 package com.example.myspringapp;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,33 +8,14 @@ import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
-@Entity
+@Entity // This tells Hibernate to make a table out of this class
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
-	@Column(name = "user_name", nullable = false, length = 50)
-	private String userName;
+	private String name;
 
-	// 空のコンストラクタ
-	public User() {
-	}
+	private String email;
 
-	//	// Getter、Setter
-	//	public Integer getId() {
-	//		return id;
-	//	}
-	//
-	//	public void setId(Integer id) {
-	//		this.id = id;
-	//	}
-	//
-	//	public String getUserName() {
-	//		return userName;
-	//	}
-	//
-	//	public void setUserName(String userName) {
-	//		this.userName = userName;
-	//	}
 }
